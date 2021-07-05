d0128811e22f9ee4153f987f299f4e53ce7e932b
Jekyll <dudrlf1859@naver.com>
2021-07-05 13:37:56 +0900

    obsecure super heroes done

--- Example/most_obsecure_superhero.py
+++ Example/most_obsecure_superhero.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.functions import col
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
 session = SparkSession.builder.appName("most_obsecure_superhero").getOrCreate()
@@ -12,4 +14,14 @@ schema = StructType(
 )
 
 heroes = session.read.option("sep", " ").schema(schema).csv("../src/resources/Marvel-names.txt")
-heroes.show()
+lines = session.read.text("../src/resources/Marvel-graph.txt")
+
+connections = lines.withColumn("id", F.split(F.col("value"), " ")[0]) \
+    .withColumn("connections", F.size(F.split(F.col("value"), " ")) - 1) \
+    .groupBy("id") \
+    .agg(F.sum("connections").alias("connections"))
+
+min_connections_count = connections.agg(F.min("connections")).first()[0]
+min_connections = connections.where(col("connections") == min_connections_count)
+result_df = min_connections.join(heroes, "id")
+result_df.show()
